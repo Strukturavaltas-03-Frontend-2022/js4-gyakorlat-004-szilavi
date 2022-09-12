99bd1742d0f70ca34d360a801e996d7791e71969
k3csk3 <szilagyi.viktor89@gmail.com>
2022-09-12 08:49:25 +0200

    kész

--- 15-generators/exercises/01/solution/js/indexGenerator.js
+++ 15-generators/exercises/01/solution/js/indexGenerator.js
@@ -1,1 +1,9 @@
+function* indexGenerator() {
+  let i = 1;
+  while (true) {
+    yield i;
+    i++;
+  }
+}
+
 export default indexGenerator;
--- 15-generators/exercises/02/solution/js/powersOfTwo.js
+++ 15-generators/exercises/02/solution/js/powersOfTwo.js
@@ -1,1 +1,9 @@
+function* powersOfTwo() {
+  let number = 2;
+  while (true) {
+    yield number;
+    number *= 2;
+  }
+}
+
 export default powersOfTwo;
